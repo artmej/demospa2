58c3e84c9296dc0901e19be12fe5158ada862507
Arturo Mejia <artmej@microsoft.com>
2023-03-28 13:32:01 -0600

    test api

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         <h1>Gracias por Contactarnos</h1>
+        <p>{{ message }}</p>
     </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/info")).json();
+    this.message = text;
+  }
+};
+</script>
